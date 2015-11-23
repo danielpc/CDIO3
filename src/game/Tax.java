86910d8002a347cd9e6313a1f7e0d26d26e4a23e
Rikke <Rikke@10.16.131.94>
2015-11-23 12:20:09 +0100

    Tax class updated

--- src/game/Tax.java
+++ src/game/Tax.java
@@ -1,18 +1,35 @@
 package game;
 
+import desktop_resources.GUI;
+
 public class Tax extends Field {
 	private int taxAmount;
-	private int taxRate = -1;
+	private int taxRate;
 
-	public Tax(String name, int taxAmount) {
+	public Tax(String name, int taxAmount, int taxRate) {
 		super(name);
 		this.taxAmount = taxAmount;
+		this.taxRate = taxRate;
 	}
 
 	@Override
 	public void land(Player p) {
-		// TODO Auto-generated method stub
+		if (taxRate < 0) {
+			GUI.showMessage("Du er landet på Tax, du skal betale " + taxAmount + "kr");
+			p.changeBalance(-taxAmount);
+		} 
+		else {
+			String s = GUI.getUserButtonPressed("Du er landet på Tax, vælg mellem følgende: ", taxAmount + " kr",
+					taxRate + "%");
+			if (s.equals(taxAmount + " kr")) {
+				p.changeBalance(-taxAmount);
+			}
+			else {
+				int amount = (int)Math.ceil((double)p.getBalance()*(taxRate/100.0));
+				p.changeBalance(-amount);
+				GUI.showMessage("Du har betalt " + amount + " kr");
+			}
+		}
 
 	}
-
 }
